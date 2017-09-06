b77b219dd15f357249906bca1405dda947305ff1
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-09-06 14:39:18 +0200

    I have started on the date

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,15 @@ class DateTimeView {
 
 
 	public function show() {
+		$nameOfDay = date("l");
+		$dateOfDay = date("jS");
+		$month = date("F");
+		$year = date("o");
+		$time = date("G:i:s");
 
-		$timeString = 'TODO, Write servertime here...';
+
+		//$timeString = date("l \\t\\h\\e jS \of F Y, \\T\\h\\e \\t\\i\\m\\e  \\i\\s G:i:s");
+		$timeString = $nameOfDay . " the " . $dateOfDay . " of " . $month . " " . $year . ", The time is " . $time;
 
 		return '<p>' . $timeString . '</p>';
 	}
