00101603c06a4e4cf69c5cec8244953f0c78b093
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-09-25 14:53:36 +0200

    I can check password and username when register new user

--- model/registerModel.php
+++ model/registerModel.php
@@ -6,7 +6,13 @@ class registerModel{
             $message = "Username has too few characters, at least 3 characters. <br> Password has too few characters, at least 6 characters.";
         } else if ($this->checkPasswordRegister()){
             $message =  "Password has too few characters, at least 6 characters.";  
-        }     
+        } else if ($this->checkUsernameRegister()){
+            $message = "Username has too few characters, at least 3 characters.";
+        } else if ($this->checkPasswordLength()){
+            $message = "Password has too few characters, at least 6 characters.";
+        } else if($this->checkPasswordRepeat()){
+            $message = "Passwords do not match.";
+        }
     }
 
     public function getUsernameLength(){
@@ -14,9 +20,24 @@ class registerModel{
 		return strlen($username);
     }
 
+    public function getPasswordLength(){
+        $password = (isset($_POST["RegisterView::Password"]) ? $_POST["RegisterView::Password"] : null);
+		return strlen($password);
+    }
+
     private function getUsernameRegister() {          
         return $_SESSION['usernameValueRegister'] = $_POST['RegisterView::UserName'];
-	}
+    }
+    
+    private function getPasswordRegister(){
+        $password = $_POST["RegisterView::Password"];
+		return $password;
+    }
+
+    private function getPasswordRepeat(){
+        $password = $_POST["RegisterView::PasswordRepeat"];
+		return $password;
+    }
 
     public function registerFail(){
         if (isset($_POST["RegisterView::Register"])){
@@ -37,6 +58,42 @@ class registerModel{
            } else {
                return false;
            }
+        }
+    }
+
+    public function checkUsernameRegister(){
+        if (isset($_POST["RegisterView::Register"])){
+            if($this->getUsernameLength() < 3 && $this->getPasswordRegister() ==
+            $this->getPasswordRepeat()){
+                    $this->getUsernameRegister();
+                    return true;
+                } 
+        } else {
+            return false;
+        }
+    }
+
+    public function checkPasswordLength(){
+        if (isset($_POST["RegisterView::Register"])){
+            if ($this->getUsernameLength() >= 3 && $this->getPasswordLength() <= 6 
+                && $this->getPasswordRegister() == $this->getPasswordRepeat()){
+                    $this->getUsernameRegister();
+                    return true;
+                }
+        } else {
+            return false;
+        }
+    }
+
+    public function checkPasswordRepeat(){
+        if(isset($_POST["RegisterView::Register"])){
+            if($this->getUsernameLength() >=3 && $this->getPasswordLength() >= 6 &&
+                $this->getPasswordRegister() != $this->getPasswordRepeat()){
+                    $this->getUsernameRegister();
+                    return true;
+                }
+        } else {
+            return false;
         }
     }
 
